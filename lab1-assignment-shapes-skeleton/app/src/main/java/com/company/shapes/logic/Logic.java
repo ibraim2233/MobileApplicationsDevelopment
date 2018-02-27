5f6c04836b53d2fc6c94f6994774fd3754376877
IBRAIM-G <ablyalimov.i.s.i.1.16@gmail.com>
2018-02-27 21:34:28 +0300

    Ablyalimov Ibraim i-1-16 lab1

--- lab1-assignment-shapes-skeleton/app/src/main/java/com/company/shapes/logic/Logic.java
+++ lab1-assignment-shapes-skeleton/app/src/main/java/com/company/shapes/logic/Logic.java
@@ -1,7 +1,7 @@
 package com.company.shapes.logic;
 
 import com.company.shapes.ui.OutputInterface;
-
+//import static java.lang.Math.*;
 /**
  * This is where the logic of this App is centralized for this assignment.
  * <p>
@@ -160,6 +160,35 @@ public class Logic
         }
     }
 
-    // TODO -- add your code here
-
+    public static double rectangleArea(double length, double width){
+        return(length*width);
+    }
+    public static double rectanglePerimeter(double length, double width){
+        return (2*(length+width));
+    }
+    public static double circleArea(double radius){
+        return(Math.PI*radius*radius);
+    }
+    public static double circleCircumference(double radius){
+        return(2*Math.PI*radius);
+    }
+    public static double rightTriangleArea(double base, double height){
+        return(0.5*base*height);
+    }
+    public static double rightTrianglePerimeter(double base, double height){
+        double c = Math.sqrt(base*base + height*height);
+        return(base+height+c);
+    }
+    public static double boxVolume(double length, double width, double depth){
+        return(length*width*depth);
+    }
+    public static double boxSurfaceArea(double length, double width, double depth){
+        return(2*(length*width+length*depth+width*depth));
+    }
+    public static double sphereVolume(double radius) {
+        return((4.0/3.0)*Math.PI*radius*radius*radius);
+    }
+    public static double sphereSurfaceArea(double radius){
+        return(4*Math.PI*radius*radius);
+    }
 }
